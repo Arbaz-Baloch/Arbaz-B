e230f9cf8f8295c058a8056ba09fb3d15418d8ac
Arbaz-Baloch <arbazbalochhhh@gmail.com>
2024-11-13 11:30:15 +0000

    Variables files.

---
+++ variables.js
@@ -0,0 +1,21 @@
+const accountID = 1234 
+
+// Once const keyword has been given or assigned when time than it cannot be changed.
+
+let accountEmail = "Arbaz123@gmail.com"
+
+var accountPassword = "Arbaz"  
+
+/* Please dont use war becouse issue in functional scope and other as well and block scope as well */
+
+accountCity = "Lahore"
+
+// accountID = 12 //Not allowed because const has been already declare in first line.
+
+accountEmail = "Sher1234@gmail.com"
+
+accountPassword = "1249"
+
+console.log (accountID)
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
