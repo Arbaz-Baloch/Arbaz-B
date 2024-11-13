5c56e50946dce7798dbba92029ccceb08670d756
Arbaz-Baloch <arbazbalochhhh@gmail.com>
2024-11-13 12:02:26 +0000

    Datatypes

---
+++ DataType.js
@@ -0,0 +1,37 @@
+"Use Strict"; // If we use that line than we will be using javascript newer version
+
+alert(3+3) // This syntex will note be used because we are using nodejs not browser for arthemeric etc +- etc.
+
+console.log( 3+
+    3 ) // Code readability should be high we cannot do that mistake again and again.
+
+console.log ("Arbaz") //tc39 for alert documentation
+
+let name ="Arbaz" //Strings date type
+
+let age = 18  // Numbers Data Types
+
+let isloggedIn = yes/No  //Bullian DataTypes
+
+// Number => 2 to pwer of 53 its a range
+
+//bigint =Used for biggest numbers like in stoke website
+
+//string = Double quote /Single quote
+
+//Bullion = Yes or no . User logged in or not.
+
+//null= Its an stand alone value.
+
+//Undefined = For no value . null etc no value assigned
+
+//symbol for unique symbols 
+
+
+//objects
+
+console.log (typeof "Arbaz") //It will show you the data types like strings
+
+console.log (typeof age);
+
+console.log (typeof null);// Its an language error or object 
